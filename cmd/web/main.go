69b9b51c043ea8c71f554f5499356df19fa43ede
atharva <a@peace>
2025-04-12 18:46:03 +0530

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+		"crypto/tls"
+		"database/sql"
+		"flag"
+		"html/template"
+    "log"
+    "net/http"
+		"os"
+		"time"
+
+		"snippetbox.basic/pkg/models/mysql"
+
+		_ "github.com/go-sql-driver/mysql"
+		"github.com/golangcollege/sessions"
+)
+
+type application struct {
+		errorLog *log.Logger
+		infoLog *log.Logger
+		session *sessions.Session
+		snippets *mysql.SnippetModel
+		templateCache map[string]*template.Template 
+}
+
+func main() {
+		addr := flag.String("addr", ":4000", "HTTP network address")
+		dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "Mysql data source name")
+		flag.Parse()
+		secret := flag.String("secret", "web:pass@/snippetbox?parseTime=true", "Secret key")
+		flag.Parse()
+		infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+		errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+		db, err := openDB(*dsn)
+		if err != nil{
+			errorLog.Fatal(err)
+		}
+		defer db.Close()
+		templateCache, err := newTemplateCache("./ui/html/")
+		if err != nil {
+				errorLog.Fatal(err)
+		}
+		session := sessions.New([]byte(*secret))
+		session.Lifetime = 12 * time.Hour
+		session.Secure = true
+		app := &application{
+				errorLog: errorLog,
+				infoLog: infoLog,
+				session: session,
+				snippets: &mysql.SnippetModel{DB: db},
+				templateCache: templateCache,
+		}
+		tlsConfig := &tls.Config {
+				PreferServerCipherSuites: true,
+				CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+		}
+		srv := &http.Server{
+				Addr: *addr,
+				ErrorLog: errorLog,
+				Handler: app.routes(),
+				TLSConfig: tlsConfig,
+				IdleTimeout: time.Minute,
+				ReadTimeout: 5 * time.Second,
+				WriteTimeout: 10 * time.Second,
+		}
+
+    infoLog.Printf("Starting server on %s", *addr)
+    err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+    errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error){
+		db, err := sql.Open("mysql", dsn)
+		if err != nil{
+				return nil, err
+		}
+		if err = db.Ping(); err != nil{
+				return nil, err 
+		}
+		return db, nil
+}
