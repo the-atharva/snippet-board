06dc7ae2f1a2ab6b9ed50493c9a2820608c9f47a
atharva <atharva.prabhune2021@gmail.com>
2025-04-20 11:00:07 +0530

    Send response instead of redirect

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-		"io/ioutil"
-		"log"
 		"net/http" 	
-		"net/http/httptest"
 		"testing"
 )
 
 func TestPing(t *testing.T) {
-		app := &application {
-				errorLog: log.New(ioutil.Discard, "", 0),
-				infoLog: log.New(ioutil.Discard, "", 0),
-		}
-		ts := httptest.NewTLSServer(app.routes())
+		app := newTestApplication(t)
+		ts := newTestServer(t, app.routes())
 		defer ts.Close()
-		rs, err := ts.Client().Get(ts.URL + "/ping")
-		if err != nil {
-				t.Fatal(err)
-		}
-		if rs.StatusCode != http.StatusOK {
-				t.Errorf("Expected %d. Got %d", http.StatusOK, rs.StatusCode)
-		}
-		defer rs.Body.Close()
-		body, err := ioutil.ReadAll(rs.Body)
-		if err != nil {
-				t.Fatal(err)
+		code, _, body := ts.get(t, "/ping")
+		if code != http.StatusOK {
+				t.Errorf("Expected %d. Got %d", http.StatusOK, code)
 		}
 		if string(body) != "OK" {
 				t.Errorf("Expected %q", "OK")
